d35f735b4f923a2c12fc0de483de401d5246ecf7
Miro Svrtan <miro@mirosvrtan.me>
2018-01-12 17:02:32 +0100

    Add source code (with spec & tests)

---
+++ src/Tag/TagName.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevboardLib\Git\Tag;
+
+use Git\Reference\ReferenceName;
+
+/**
+ * @see \spec\DevboardLib\Git\Tag\TagNameSpec
+ * @see \Tests\DevboardLib\Git\Tag\TagNameTest
+ */
+class TagName implements ReferenceName
+{
+    const TYPE = 'Tag';
+
+    /** @var string */
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getValue(): string
+    {
+        return $this->name;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    public function serialize(): string
+    {
+        return $this->name;
+    }
+
+    public static function deserialize(string $name): self
+    {
+        return new self($name);
+    }
+}
